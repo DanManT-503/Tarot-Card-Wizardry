601dd7b250d49b6accf499e0b4ef4c06801dbfc8
DanManT-503 <52472086+DanManT-503@users.noreply.github.com>
2020-06-16 00:59:03 -0600

    shit

--- src/main/java/com/danmant/tarotwizardry/TarotCardWizardry.java
+++ src/main/java/com/danmant/tarotwizardry/TarotCardWizardry.java
@@ -7,11 +7,11 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = ExampleMod.MODID, name = ExampleMod.NAME, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = com.example.examplemod.TarotCardWizardry.MODID, name = com.example.examplemod.TarotCardWizardry.NAME, version = com.example.examplemod.TarotCardWizardry.VERSION)
+public class TarotCardWizardry
 {
-    public static final String MODID = "examplemod";
-    public static final String NAME = "Example Mod";
+    public static final String MODID = "tarotwizardry";
+    public static final String NAME = "Tarot Card Wizardry";
     public static final String VERSION = "1.0";
 
     private static Logger logger;
